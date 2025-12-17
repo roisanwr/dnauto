d05396a743a241bfa226d728af6a9ace9d7d36ef
Nisrina <thifalufaira@gmail.com>
2025-12-17 13:43:44 +0700

    update env & seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Database\Seeders;
+// database/seeders/DatabaseSeeder.php
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash; // <<< Tambahkan ini
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // 1. User Admin (untuk testing)
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin DN Auto',
+            'email' => 'dnautocompany@gmail.com', // Email Admin
+            'password' => Hash::make('DNAUTO123'), // Password Admin
+            'role' => 'admin', // <<< JADIKAN ADMIN
         ]);
+
+        // 2. Contoh User Biasa (Customer)
+        User::factory()->create([
+            'name' => 'Test Customer',
+            'email' => 'customer@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'customer',
+        ]);
+        
+        // Opsional: Buat 10 customer dummy lainnya
+        // User::factory(10)->create(['role' => 'customer']);
     }
 }
