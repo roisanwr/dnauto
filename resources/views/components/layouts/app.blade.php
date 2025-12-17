ce355701ee8307e1f01d1f51d483c246e7421595
roisanwr <roisanwar44@gmail.com>
2025-12-17 13:39:09 +0700

    baru

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -72,7 +72,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
 
-                    <span class="ml-3 whitespace-nowrap" x-show="sidebarOpen">Riwayat Pesanan</span>
+                    <span class="ml-3 whitespace-nowrap" x-show="sidebarOpen">Pesanan Saya</span>
                 </a>
             </nav>
 
